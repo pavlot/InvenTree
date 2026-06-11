eaf8d3b8a2462016a92eb8e7066ebd2172a15533
Pavlo Taranov <taranov.pavel@gmail.com>
2026-06-11 18:49:44 +0200

    Fix for quantity more than 15 digits

--- src/backend/InvenTree/stock/serializers.py
+++ src/backend/InvenTree/stock/serializers.py
@@ -1612,9 +1612,12 @@ class StockAdjustmentItemSerializer(serializers.Serializer):
 
         return stock_item
 
-    quantity = serializers.DecimalField(
-        max_digits=15, decimal_places=5, min_value=Decimal(0), required=True
-    )
+    quantity = serializers.FloatField(required=True)
+
+    def validate_quantity(self, quantity):
+        """Validate the quantity value."""
+        decimal_quantity = round(quantity, 5)
+        return decimal_quantity
 
     def validate_quantity(self, quantity):
         """Validate the quantity value."""
